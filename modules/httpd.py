ebefa063b865350ec956951cdd04fed939903a6c
Sam Shelton <sam@shelt.ca>
2015-09-10 18:44:17 -0500

    Seperate path into parts

--- modules/httpd.py
+++ modules/httpd.py
@@ -14,6 +14,7 @@ from modules import retrieve
 
 DEFAULT_PORT = 34989
 ROOT_REDIRECT = "/box/in"
+ATTACHMENT_DIRECTORY = "attachments" #TODO make this come from the same config as the one in htgen
 
 # Content-Types associated with extensions
 CTYPE_PATH = {
@@ -73,41 +74,45 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         length = int(self.headers['Content-Length'])
         post_data = parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-        print(post_data) #todo
     
         
     def do_GET(self):
-        request = parse.urlparse(self.path)
-        params = parse.parse_qs(request.query)
-        path = request.path.rstrip("/") # So it's just "static/..."
+        _request = parse.urlparse(self.path)
+        params = parse.parse_qs(_request.query)
+        fullpath = _request.path.strip("/")
+        path = fullpath.split("/")
 
         if params:
             if "refresh" in params:
                 retrieve.retrieve()
-                self.respond(302,[("Location",path)])
+                self.respond(302,[("Location",fullpath)])
                 return
         # Root redirect
-        if path == "":
+        if fullpath == "":
             self.respond(302,[("Location",ROOT_REDIRECT)])
             return
         #################
         # File handling #
         #################
-        if path.startswith("/static/") or path.startswith("/attachment/") and is_file(path):
-            print("GET "+path)
-            path = path.lstrip("/")
+        is_attachment_request = path[0] == ATTACHMENT_DIRECTORY
+        is_static_request = path[0] == "static"
+        if (is_static_request or is_attachment_request) and is_file(fullpath):
+            print("GET "+fullpath) # debug
 
             # Get Content-Type
-            _,ext = os.path.splitext(path)
+            _,ext = os.path.splitext(fullpath)
             # Open the file
             if CTYPE_ISBINARY[ext]:
-                f = open(path, 'rb')#, errors='ignore')
+                f = open(fullpath, 'rb')#, errors='ignore')
             else:
-                f = open(path)
+                f = open(fullpath)
             
             # Respond
+            headers = [("Content-type",CTYPE_PATH[ext])]
+            if is_attachment_request:
+                headers.append(("Content-Disposition","attachment"))
             try:
-                self.respond(200, [("Content-type",CTYPE_PATH[ext])], CTYPE_ISCACHE[ext])
+                self.respond(200, headers, CTYPE_ISCACHE[ext])
             except KeyError:
                 print("! Unhandled extension: "+ext)
                 f.close()
@@ -123,18 +128,20 @@ class RequestHandler(BaseHTTPRequestHandler):
         #################
         # *box handling #
         #################
-        elif path.startswith("/box/"):
+        elif path[0] == "box":
             self.respond(200,[("Content-type","text/html")])
-            htgen.box(self.wfile, path[5:]) # 5 is the length of "/box/"
+            htgen.box(self.wfile, path[1])
         
         ###################
         # Thread handling #
         ###################
-        elif path.startswith("/thread/"):
+        elif path[0] == "thread":
             self.respond(200,[("Content-type","text/html")])
-            htgen.thread(self.wfile, parse.unquote(path[8:])) # 8 is the length of "/thread/"
-
-        elif path == "/compose":
+            htgen.thread(self.wfile, parse.unquote(path[1]))
+        elif path[0] == "raw":
+            self.respond(200,[("Content-type","text/plain")])
+            wfile.write(database.get_message(msgid))
+        elif path[0] == "compose":
             self.respond(200,[("Content-type","text/html")])
             # Todo params parsing
             htgen.compose(self.wfile, recips=[], sender="", inreplyto="<1557752776.152901.1441738798174.JavaMail.open-xchange@app4.ox.privateemail.com>", replyall=False)
